31d06f7f634ce897e3d93d0ec80e32821ef62ca9
David Mena <davidmenam15@gmail.com>
2020-11-29 18:03:10 -0500

    Feat: Dummy implementation of the intelligent vacuum cleaner

--- src/myagent/VacAgent.java
+++ src/myagent/VacAgent.java
@@ -8,11 +8,8 @@ package myagent;
 import agent.Action;
 import agent.Agent;
 import agent.Percept;
-import vacworld.ShutOff;
-import vacworld.SuckDirt;
-import vacworld.TurnLeft;
-import vacworld.TurnRight;
-import vacworld.VacPercept;
+import vacworld.*;
+import java.util.Random;
 
 /* Change the code as appropriate.  This code
    is here to help you understand the mechanism
@@ -26,9 +23,10 @@ public class VacAgent extends Agent {
     private boolean dirtStatus = true;
     private boolean bumpFeltInPrevMove = true;
     private boolean obstacleInFront = true;
+    private int timesForward = 0;
 
     public void see(Percept p) {
-
+        System.out.println(p.toString());
         VacPercept vp = (VacPercept) p;
         dirtStatus = vp.seeDirt();
         bumpFeltInPrevMove = vp.feelBump();
@@ -36,19 +34,46 @@ public class VacAgent extends Agent {
     }
 
     public Action selectAction() {
-        Action action = new SuckDirt();
+        Action action = new GoForward();
         SuckDirt suckDirt = new SuckDirt();
         TurnLeft turnLeft = new TurnLeft();
         TurnRight turnRight = new TurnRight();
         ShutOff shutOff = new ShutOff();
-        
+
+        System.out.println("dirtStatus");
+        System.out.println(dirtStatus);
+        System.out.println("bumpFeltInPrevMove");
+        System.out.println(bumpFeltInPrevMove);
+        System.out.println("obstacleInFront");
+        System.out.println(obstacleInFront);
+
+        // make corresponding action
         if(dirtStatus)
             action = suckDirt;
         if(bumpFeltInPrevMove)
               action = turnLeft;
         if(obstacleInFront)
               action = turnRight;
-        
+        // in case too many times going forward
+        System.out.println(action.getClass().getName());
+        if(action.getClass().getName() == "vacworld.GoForward"){
+            System.out.println("Test");
+            timesForward++;
+        }
+        // turn left or right
+        if(timesForward == 2){
+            Random rd = new Random();
+            boolean option = rd.nextBoolean();
+            if( option == true){
+                action = new TurnLeft();
+            }
+            else {
+                action = new TurnRight();
+            }
+            // reinitialize counter
+            timesForward = 0;
+        }
+
         return action;
     }
 
